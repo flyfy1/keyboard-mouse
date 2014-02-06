3b71419fbdaa5c293f8edeeeaf8cf1af613285c3
Song Yangyu <flyfy1@gmail.com>
2014-02-07 04:04:39 +0800

    Basic example for global-short-key

--- src/main/java/me/songyy/App.java
+++ src/main/java/me/songyy/App.java
@@ -1,5 +1,13 @@
 package me.songyy;
 
+import java.awt.event.KeyEvent;
+
+import javax.swing.KeyStroke;
+
+import com.tulskiy.keymaster.common.HotKey;
+import com.tulskiy.keymaster.common.HotKeyListener;
+import com.tulskiy.keymaster.common.Provider;
+
 /**
  * Hello world!
  *
@@ -8,6 +16,18 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Provider provider = Provider.getCurrentProvider(false);
+    	
+    	HotKeyListener keyListener = new HotKeyListener() {
+			@Override
+			public void onHotKey(HotKey key) {
+				System.out.println("Hot Key Pressed: " + key);
+			}
+		};
+    	
+		provider.register(KeyStroke.getKeyStroke('M',KeyEvent.SHIFT_DOWN_MASK | KeyEvent.CTRL_DOWN_MASK),keyListener);
+    	provider.register(KeyStroke.getKeyStroke('A',KeyEvent.SHIFT_DOWN_MASK | KeyEvent.CTRL_DOWN_MASK,true),keyListener);
+    	provider.register(KeyStroke.getKeyStroke('M',KeyEvent.SHIFT_DOWN_MASK | KeyEvent.CTRL_DOWN_MASK,true),keyListener);
+    	
     }
 }
